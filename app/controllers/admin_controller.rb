3072d2616af076265e88562201356e8e2f5b3770
n-katou <102382177+n-katou@users.noreply.github.com>
2024-04-22 01:38:39 +0900

    レイアウト調整

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,17 +2,24 @@ class AdminController < ApplicationController
   before_action :authenticate_admin!
   
   def index
-    @users = User.all  # 全ユーザーを取得
+    @users = User.order(:id)  # ユーザーをIDで昇順に並べる
   end
 
   def reset_vote_count
     user = User.find_by(id: params[:user_id])  # ユーザーIDで検索
     if user
-      user.update(vote_count: 0)  # 投票数をリセット
+      # ユーザーの投票を全て削除
+      Vote.where(user_id: user.id).destroy_all
+  
+      # 投票数をリセット
+      user.update(vote_count: 0)
+  
       flash[:notice] = "ユーザー#{user.name}の投票数をリセットしました。"
     else
       flash[:alert] = "ユーザーが見つかりません。"
     end
+  
+    # 管理画面にリダイレクト
     redirect_to admin_path
   end
 end
