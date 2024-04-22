0aa3c4a419e3197b0648726e9349e990b086567e
n-katou <102382177+n-katou@users.noreply.github.com>
2024-04-23 00:06:11 +0900

    レイアウト修正

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,33 +2,33 @@ class VotesController < ApplicationController
   before_action :authenticate_user!  # ユーザー認証が必要な場合
   
   def vote
-    # idで映画を検索
+    # 映画をidで検索
     @movie = Movie.find_by(id: params[:id])
     
     if @movie.nil?
-      flash[:alert] = "映画が見つかりません。"
+      flash[:alert] = t('movies.not_found')  # 映画が見つからない場合のメッセージ
       redirect_to movies_path
       return
     end
     
-    # ユーザーの全体の投票回数を確認
+    # ユーザーの総投票数を確認
     user_total_votes = Vote.where(user_id: current_user.id).count
     
     # ユーザーが特定の映画に投票しているか確認
     user_movie_votes = Vote.where(user_id: current_user.id, movie_id: @movie.id).count
     
     if user_total_votes >= 3
-      flash[:alert] = "You can only vote 3 times."
+      flash[:alert] = t('votes.limit_reached')  # 投票制限を超えた場合のメッセージ
     elsif user_movie_votes > 0
-      flash[:alert] = "You have already voted for this movie."
+      flash[:alert] = t('votes.already_voted')  # 特定の映画に既に投票している場合
     else
       # 投票を記録
       Vote.create(user_id: current_user.id, movie_id: @movie.id)
       
-      # 投票数を増加
-      current_user.increment!(:vote_count)  # 投票数を＋1
-    
-      flash[:notice] = "You have voted for #{@movie.title} (#{user_total_votes + 1}/3)"
+      # ユーザーの投票数を増加
+      current_user.increment!(:vote_count)
+      
+      flash[:notice] = t('votes.success', movie_title: @movie.title, current_count: user_total_votes + 1)  # 投票成功時のメッセージ
     end
     
     # 映画のページにリダイレクト
