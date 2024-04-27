4fe9303fb10369d91a555cdf27ba884089284fda
n-katou <102382177+n-katou@users.noreply.github.com>
2024-04-27 19:50:52 +0900

    googleログイン動作確認

--- app/controllers/google_oauths_controller.rb
+++ app/controllers/google_oauths_controller.rb
@@ -3,27 +3,26 @@ class GoogleOauthsController < ApplicationController
   def oauth
     client_id = ENV['GOOGLE_CLIENT_ID']
     redirect_uri = "https://conancinemareviews.onrender.com/oauth/callback?provider=google"
+    # 開発環境の場合、以下を使用
     # redirect_uri = "http://localhost:3000/oauth/callback?provider=google"
     scope = "email profile"
     state = "SOME_STATE_VALUE"
 
     oauth_url = "https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=#{client_id}&redirect_uri=#{CGI.escape(redirect_uri)}&scope=#{CGI.escape(scope)}&state=#{state}&access_type=offline&prompt=consent"
-    redirect_to oauth_url, allow_other_host: true
+    redirect_to oauth_url, allow_other_host: true  # 他のホストへのリダイレクトを許可
   end
 
   def callback
     service = GoogleOauthService.new(params[:code])
-    @user = service.authenticate
+    user = service.authenticate  # `@user`から`user`に変更
 
-    if @user
-      reset_session
-      auto_login(@user)
-      redirect_to root_path, notice: t('auth.login_success')
+    if user
+      reset_session  # セッションをリセット
+      sign_in(user)  # ユーザーをDeviseでログイン
+      redirect_to root_path, notice: 'ログインに成功しました！'  # 成功時のリダイレクト
     else
-      Rails.logger.error { "ログイン処理中にエラーが発生しました。" }
-      redirect_to login_path, alert: t('auth.login_failed')
+      Rails.logger.error("ログイン処理中にエラーが発生しました。")  # エラー時のログ出力
+      redirect_to new_user_registration_path, alert: 'ログインに失敗しました。'  # 失敗時のリダイレクト
     end
   end
-
-  private
 end
